fc13a5f06951d673c61934d9967296192d5624ac
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-27 14:35:05 +0100

    nix: add build with eigen 5, ref #426

--- flake.nix
+++ flake.nix
@@ -11,30 +11,69 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
         {
+          pkgs,
+          pkgs-eigen_5,
+          self',
+          system,
+          ...
+        }:
+        {
+          _module.args =
+            let
+              proxsuiteOverlay = final: prev: {
+                proxsuite = prev.proxsuite.overrideAttrs {
+                  src = final.lib.fileset.toSource {
+                    root = ./.;
+                    fileset = final.lib.fileset.unions [
+                      ./benchmark
+                      ./bindings
+                      ./cmake-external
+                      ./CMakeLists.txt
+                      ./doc
+                      ./examples
+                      ./include
+                      ./package.xml
+                      ./test
+                    ];
+                  };
+                  postPatch = "";
+                };
+              };
+              eigen5Overlay = final: prev: {
+                eigen = prev.eigen.overrideAttrs (super: rec {
+                  version = "5.0.0";
+                  src = final.fetchFromGitLab {
+                    inherit (super.src) owner repo;
+                    tag = version;
+                    hash = "sha256-L1KUFZsaibC/FD6abTXrT3pvaFhbYnw+GaWsxM2gaxM=";
+                  };
+                  patches = [ ];
+                  postPatch = "";
+                });
+              };
+            in
+            {
+              pkgs = import inputs.nixpkgs {
+                inherit system;
+                overlays = [ proxsuiteOverlay ];
+              };
+              pkgs-eigen_5 = import inputs.nixpkgs {
+                inherit system;
+                overlays = [
+                  eigen5Overlay
+                  proxsuiteOverlay
+                ];
+              };
+            };
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
           packages = {
             default = self'.packages.proxsuite;
-            proxsuite = pkgs.python3Packages.proxsuite.overrideAttrs {
-              src = pkgs.lib.fileset.toSource {
-                root = ./.;
-                fileset = pkgs.lib.fileset.unions [
-                  ./benchmark
-                  ./bindings
-                  ./cmake-external
-                  ./CMakeLists.txt
-                  ./doc
-                  ./examples
-                  ./include
-                  ./package.xml
-                  ./test
-                ];
-              };
-            };
+            proxsuite = pkgs.python3Packages.proxsuite;
+            proxsuite-eigen_5 = pkgs-eigen_5.python3Packages.proxsuite;
           };
         };
     };
